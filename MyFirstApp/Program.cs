8468980706587f92edd3cca0e06fa8b1e958b2a7
Cryslin Sinimäe <csinimae@tktk.ee>
2024-03-25 02:29:58 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,14 @@
+﻿//This is a comment to my code
+
+//Rakendus küsib kasutajalt sisestada nime
+//Rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:");
+//string=sõne
+string userName;
+
+userName = Console.ReadLine();
+
+Console.WriteLine("Hello" + "," + userName);
+
+Console.WriteLine($"Hello, {userName} !"); 
